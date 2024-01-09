058159dfac564ae428290e5e16cb128dd80e74be
Dwaipayan Mazumder <85861149+Dwaipayan-Maz@users.noreply.github.com>
2024-01-09 17:34:34 +0000

    TypeOf Func

--- start.js
+++ start.js
@@ -1,1 +1,13 @@
-console.log("Suru hok tobe")
+/*console.log("Suru hok tobe")
+bg="Green"
+const alo= bg
+const rol=83
+let age=18
+
+var home="Kolkata"
+hobby="football"
+let style;
+console.log(alo)
+console.table([age,nation,home,hobby,style])*/
+let nation= 891;
+console.log(typeof 891);
